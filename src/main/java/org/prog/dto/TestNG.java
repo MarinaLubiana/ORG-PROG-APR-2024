9f451640d1fdfcf7917593cb918241769e64cc6c
Marina <marina.lubyana2016@gmail.com>
2024-05-28 12:33:18 +0300

    HomeWork7

---
+++ src/main/java/org/prog/dto/TestNG.java
@@ -0,0 +1,11 @@
+package org.prog.dto;
+
+public class TestNG {
+    @org.jetbrains.annotations.NotNull
+    private String
+    getRandomFirstLastName() {
+        ResultsDto.class;
+        NameDto name = dto.getResults().get(0).getName();
+        return name.getFirst() + " " + name.getLast();
+        return name.getClass() + " " + name.getLast(); }}
+}
